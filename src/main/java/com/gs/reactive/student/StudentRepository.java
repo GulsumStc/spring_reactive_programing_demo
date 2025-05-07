6b4d2e27331a86eac8ce706fcb9a05a6f43727d8
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2025-05-07 20:46:13 +0300

    fix: correct method name to findAllByFirstNameIgnoreCase

--- src/main/java/com/gs/reactive/student/StudentRepository.java
+++ src/main/java/com/gs/reactive/student/StudentRepository.java
@@ -5,5 +5,5 @@ import reactor.core.publisher.Flux;
 
 public interface StudentRepository extends ReactiveCrudRepository<Student, Integer> {
 
-    Flux<Student> findAllByFirstNameIgnoreCase(String firstName);
+    Flux<Student> findAllByNameIgnoreCase(String name);
 }
