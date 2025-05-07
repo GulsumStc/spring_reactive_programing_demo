7fd904ce58a70c6b08648490da158f3ebc2f4d9e
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2025-05-07 20:09:03 +0300

    feat: created project for reactive demo  with essential dependencies

---
+++ src/main/java/com/gs/reactive/student/Student.java
@@ -0,0 +1,23 @@
+package com.gs.reactive.student;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "students")
+public class Student {
+
+
+    @Id
+    private Integer id;
+    private String name;
+    private String lastName;
+    private String age;
+
+
+}
