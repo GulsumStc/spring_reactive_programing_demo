40e2a15ec632d68276ca7d6342b98c9b06d31b56
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2025-05-07 20:46:51 +0300

    feat: add CommandLineRunner to initialize sample student data

--- src/main/java/com/gs/reactive/ReactiveApplication.java
+++ src/main/java/com/gs/reactive/ReactiveApplication.java
@@ -1,13 +1,34 @@
 package com.gs.reactive;
 
+import com.gs.reactive.student.Student;
+import com.gs.reactive.student.StudentRepository;
+import com.gs.reactive.student.StudentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReactiveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentService studentService) {
+
+		return args -> {
+			for (int i = 0; i < 10; i++) {
+				studentService.save(Student.builder()
+						.name("Gulsum" + i)
+						.lastName("Satic" + i)
+						.age(String.valueOf(i))
+						.build()
+				);
+
+			}
+		};
 	}
 
 }
